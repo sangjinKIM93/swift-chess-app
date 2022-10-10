2605f3495fdd78e227baf12e3b300325ff2ac075
sangjinKIM93 <larger611@gmail.com>
2022-10-10 20:27:46 +0900

    King 추가

--- ray-chess/ray-chess/Chess/King.swift
+++ ray-chess/ray-chess/Chess/King.swift
@@ -1,18 +1,18 @@
 //
-//  Knight.swift
+//  King.swift
 //  ray-chess
 //
-//  Created by 김상진 on 2022/10/03.
+//  Created by 김상진 on 2022/10/10.
 //
 
 import Foundation
 
-class Knight: Piece {
+class King: Piece {
     init(color: Piece.Color, position: Position) {
         super.init(
             position: position,
             color: color,
-            name: color == .black ? "U+265E" : "U+2658",
+            name: color == .black ? "U+265A" : "U+2654",
             point: 3,
             maxCount: 2,
             moveType: .dot
@@ -22,30 +22,32 @@ class Knight: Piece {
     override func initializablePositions() -> [Position] {
         switch color {
         case .black:
-            return [Position(rank: .one, file: .B), Position(rank: .one, file: .G)]
+            return [Position(rank: .one, file: .D)]
         case .white:
-            return [Position(rank: .eight, file: .B), Position(rank: .eight, file: .G)]
+            return [Position(rank: .eight, file: .D)]
         }
     }
     
     override func reachablePositions() -> [Position] {
         var positions = [Position]()
-        [(2,-1), (2,1), (-2,-1), (-2,1), (1,-2), (-1,-2), (1,2), (-1,2)]
-            .forEach { (file: Int, rank: Int) in
+        Piece.Direction.allCases.forEach({ direction in
+            let file = direction.getRawValue().file
+            let rank = direction.getRawValue().rank
+            
             if let rank = position.rank.getPoint(added: rank),
                let file = position.file.getPoint(added: file) {
                 positions.append(.init(rank: rank, file: file))
             }
-        }
+        })
         return positions
     }
     
     override func getSymbol() -> String {
         switch color {
         case .white:
-            return "♘"
+            return "♔"
         case .black:
-            return "♞"
+            return "♚"
         }
     }
 }
